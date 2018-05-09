7cde2ba0d285a6a0687f8618a96777711f3485d6
suyinkaishifangle <597524105@qq.com>
2018-05-09 09:13:17 +0800

    TPP

---
+++ Assets/_Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	public float Speed = 20;
+	public Rigidbody Bullet;
+	public Transform FPonit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+
+Rigidbody clone;
+
+clone = (Rigidbody)Instantiate(Bullet,FPonit.position,FPonit.rotation);
+
+clone.velocity = transform.TransformDirection(Vector3.forward*Speed); 
+
+}
+	}
+}
